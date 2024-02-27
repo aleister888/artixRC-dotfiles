5664c39ada9428f79d814fd56d881fa2a78c8b42
aleister888 <pacoe1000@protonmail.com>
2024-02-28 00:58:18 +0100

    ;_;

--- stage1.sh
+++ stage1.sh
@@ -23,43 +23,49 @@ else
 	part_type='gpt' # GPT para UEFI
 fi
 
+# Definimos nuestras particiones
+case "$disk" in
+*"nvme"*)
+	part1="$disk"p1
+	part2="$disk"p2
+	part3="$disk"p3
+	;;
+*)
+	part1="$disk"1
+	part2="$disk"2
+	part3="$disk"3
+	;;
+esac
+
 # Creamos una partición /boot o /boot/efi dependiendo
 # de si estamos en un sistema UEFI o no
 if [ "$part_type" == "dos" ]; then
 	# BIOS -> MBR
 	parted -s "/dev/$disk" mklabel $part_type mkpart primary ext4 1MiB 513MiB set 1 boot on
-	case "$disk" in
-	*"nvme"*)
-		disk="$disk"p ;;
-	esac
-	mkfs.ext4 "/dev/${disk}1"
+	mkfs.ext4 "/dev/$part1"
 else
 	# EUFI -> GPT
 	parted -s "/dev/$disk" mklabel $part_type mkpart primary fat32 1MiB 513MiB set 1 boot on
-	case "$disk" in
-	*"nvme"*)
-		disk="$disk"p ;;
-	esac
-	mkfs.fat -F32 "/dev/${disk}1"
+	mkfs.fat -F32 "/dev/$part1"
 fi
 
 # Crear partición swap de 4GB
-parted -s /dev/$disk mkpart primary linux-swap 513MiB 4.5GB
+parted -s "/dev/$part2" mkpart primary linux-swap 513MiB 4.5GB
 
 # Partición primaria / BTRFS
-parted -s /dev/$disk mkpart primary btrfs 4.5GB 100%
-mkfs.btrfs /dev/${disk}3
+parted -s "/dev/$part3" mkpart primary btrfs 4.5GB 100%
+mkfs.btrfs "/dev/$part3"
 
 # Crear subvolúmenes para / y /home
-mount /dev/${disk}3 /mnt
+mount "/dev/$part3" /mnt
 btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
 umount /mnt
 
 # Montar subvolúmenes
-mount -o noatime,compress=zstd,subvol=@ /dev/${disk}3 /mnt
+mount -o noatime,compress=zstd,subvol=@ "/dev/$part3" /mnt
 mkdir -p /mnt/home
-mount -o noatime,compress=zstd,subvol=@home /dev/${disk}3 /mnt/home
+mount -o noatime,compress=zstd,subvol=@home "/dev/$part3" /mnt/home
 
 echo "Formateo completado."
 
