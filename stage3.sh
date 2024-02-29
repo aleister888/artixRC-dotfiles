565d427c7c24abb3655a4a054a8d4226b52bd549
aleister888 <pacoe1000@protonmail.com>
2024-02-29 01:49:48 +0100

    packages

--- stage3.sh
+++ stage3.sh
@@ -73,8 +73,8 @@ esac
 
 # Instalar paquetes del AUR
 
-user_packages="librewolf ufw ufw-openrc syslog-ng syslog-ng-openrc thunderbird thunderbird-dark-reader telegram-desktop"
-aur_packages="librewolf-extension-darkreader-bin librewolf-extension-violentmonkey-bin webcord electronmail-bin qbittorrent-qt5 tauon-music-box irqbalance-openrc"
+user_packages="transmission-gtk librewolf ufw ufw-openrc syslog-ng syslog-ng-openrc thunderbird thunderbird-dark-reader telegram-desktop"
+aur_packages="librewolf-extension-darkreader-bin librewolf-extension-violentmonkey-bin webcord-bin electronmail-bin tauon-music-box irqbalance-openrc"
 
 doas pacman -S --noconfirm $user_packages
 
