6a0936065d88129341835b16ea5a53e24a7aafcf
aleister888 <pacoe1000@protonmail.com>
2024-03-14 19:46:21 +0100

    help pdf

--- dwm/config.h
+++ dwm/config.h
@@ -186,6 +186,7 @@ static const char *spawnscratchpadcmd[] = { TERM, TERMT, "scratchpad", NULL }; /
 static const Key keys[] = {
 	// Modificador                  Tecla      Función           Argumento
 	// Abrir dmenu
+	{ MODKEY|ControlMask            XK_h       spawn,            SHCMD("zathura ~/.dotfiles/Help.pdf") }
 	{ MODKEY,                       XK_p,      spawn,            {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_p,      spawn,            SHCMD("j4-dmenu-desktop --dmenu 'dmenu -c -l 16'") },
 	// Abrir terminal
