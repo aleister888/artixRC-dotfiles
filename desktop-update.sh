57cd6cef9b11a25f8e2f5c2face2bb5778fac07d
aleister888 <pacoe1000@protonmail.com>
2024-03-27 02:27:04 +0100

    update script fix

--- desktop-update.sh
+++ desktop-update.sh
@@ -12,6 +12,19 @@ ln -s $HOME/.dotfiles/.profile $HOME/
 find "$HOME/.local/bin" -type l ! -exec test -e {} \; -delete
 find "$HOME/.config"    -type l ! -exec test -e {} \; -delete
 
+plugin_install(){
+	git clone "https://github.com/$1" "$HOME/.dotfiles/.config/zsh/$(basename "$1")"
+}
+
+[ ! -e $HOME/.config/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh ] && \
+     plugin_install zsh-users/zsh-autosuggestions
+[ ! -e $HOME/.config/zsh/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh ] && \
+     plugin_install zsh-users/zsh-history-substring-search
+[ ! -e $HOME/.config/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ] && \
+     plugin_install zsh-users/zsh-syntax-highlighting
+[ ! -e $HOME/.config/zsh/zsh-you-should-use/you-should-use.plugin.zsh ] && \
+	plugin_install MichaelAquilina/zsh-you-should-use
+
 # Actualizar plugins de zsh
 sh -c "cd $HOME/.config/zsh/zsh-autosuggestions && git pull"
 sh -c "cd $HOME/.config/zsh/zsh-history-substring-search && git pull"
--- update.sh
+++ update.sh
@@ -14,6 +14,19 @@ find "$HOME/.config"    -type l ! -exec test -e {} \; -delete
 [ ! -d "$HOME/.local/share/dwm" ] && mkdir -p "$HOME/.local/share/dwm"
 ln -s ~/.dotfiles/dwm/autostart.sh ~/.local/share/dwm/autostart.sh 2>/dev/null
 
+plugin_install(){
+	git clone "https://github.com/$1" "$HOME/.dotfiles/.config/zsh/$(basename "$1")"
+}
+
+[ ! -e $HOME/.config/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh ] && \
+     plugin_install zsh-users/zsh-autosuggestions
+[ ! -e $HOME/.config/zsh/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh ] && \
+     plugin_install zsh-users/zsh-history-substring-search
+[ ! -e $HOME/.config/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ] && \
+     plugin_install zsh-users/zsh-syntax-highlighting
+[ ! -e $HOME/.config/zsh/zsh-you-should-use/you-should-use.plugin.zsh ] && \
+	plugin_install MichaelAquilina/zsh-you-should-use
+
 # Actualizar plugins de zsh
 sh -c "cd $HOME/.config/zsh/zsh-autosuggestions && git pull"
 sh -c "cd $HOME/.config/zsh/zsh-history-substring-search && git pull"
