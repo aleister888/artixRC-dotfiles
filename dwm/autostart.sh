8e51f66003c2c7e02a48ac069f13f71edeefded5
aleister888 <pacoe1000@protonmail.com>
2024-04-02 14:59:18 +0200

    compfy

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -100,7 +100,7 @@ dbus-update-activation-environment --all
 pgrep polkit-gnome	|| /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 pgrep gnome-keyring	|| gnome-keyring-daemon -r -d &
 pgrep udiskie		|| udiskie -t -a &
-pgrep compfy		|| compfy -c -f --vsync --corner-radius 12 &
+pgrep compfy		|| compfy -c -f --vsync --corner-radius 12 --config=$HOME/.config/picom/picom.conf &
 pgrep dwmblocks		|| dwmblocks &
 if [ ! -e /sys/class/power_supply/BAT0 ]; then
 pgrep x0vncserver	|| x0vncserver -localhost -SecurityTypes none &
