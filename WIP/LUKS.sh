2811669d65f00ef515ff26f221a3d564c4d0ff1b
aleister888 <pacoe1000@protonmail.com>
2024-04-05 22:54:29 +0200

    test

--- WIP/LUKS.sh
+++ WIP/LUKS.sh
@@ -163,11 +163,11 @@ format_disks(){
 	fi
 	# Creamos nuestra tabla de particionado y partición de arranque
 	if [ "$PART_TYPE" == "msdos" ]; then # BIOS -> MBR
-		echo -e "label: dos\nstart=1MiB, size=512MiB, type=83\n" | sfdisk -f /dev/"$INSTALL_DISK" >/dev/null
+		echo -e "label: dos\nstart=1MiB, size=512MiB, type=83\n" | sfdisk -f "/dev/$ROOT_DISK" >/dev/null
 		mkfs.ext4 "/dev/$bootpart"
 	else # UEFI -> GPT
 		echo -e "label: gpt\nstart=1MiB, size=512MiB, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B\n" | \
-		sfdisk -f /dev/"$ROOT_DISK" >/dev/null; mkfs.fat -F32 "/dev/$bootpart"
+		sfdisk -f "/dev/$ROOT_DISK" >/dev/null; mkfs.fat -F32 "/dev/$bootpart"
 	fi
 	# Creamos la partición root
 	parted -s "/dev/$ROOT_DISK" mkpart primary 513MiB 100%
