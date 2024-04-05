ffe0a9dacb1392f0467d7e74ab6c6baa34d17b42
aleister888 <pacoe1000@protonmail.com>
2024-04-05 22:50:20 +0200

    test

--- WIP/LUKS.sh
+++ WIP/LUKS.sh
@@ -2,6 +2,7 @@
 
 # Instalar whiptail y parted
 pacman -Sy --noconfirm --needed parted libnewt
+
 # Detectar si el sitema es UEFI o BIOS.
 if [ ! -d /sys/firmware/efi ]; then
 	PART_TYPE="msdos" # MBR para BIOS
@@ -28,12 +29,20 @@ echo_msg(){
 	clear; echo "$1"; sleep 1
 }
 
+# Función para salirnos del script
+script_exit(){
+	whip_yes "Salir" "¿Desea cancelar la instalación? En caso contrario re-eligiera su esquema de particiones" &&
+	exit 1
+}
+
+# Función que nos muestra como quedarían las particiones de nuestra instalación para
+# confirmar los cambios. También prepara las variables para formatear los discos
 scheme_show(){
 	# Inicializamos nuestras variables
 	local scheme # Variable con el esquema de particiones completo
-	local bootmount # Punto de montaje con la partición de arranque
-	local bootpart # Partición de arranque
-	local rootpart # Partición con el sistema
+	bootmount= # Punto de montaje con la partición de arranque
+	bootpart= # Partición de arranque
+	rootpart= # Partición con el sistema
 	local roottype # Tipo de / ( Encriptado o sin encriptación)
 	# Establecemos la partición de arranque en función del tipo de sistema
 	if [ "$PART_TYPE" == "msdos" ]; then
@@ -41,52 +50,55 @@ scheme_show(){
 	else
 		bootmount="/boot/efi"
 	fi
-	# Ajustamos / en función del tipo de disco
+	# Definimos el nombre de las particiones
+	# de nuestro disco principal*
 	case "$ROOT_DISK" in
 	*"nvme"*)
 		bootpart="$ROOT_DISK"p1
 		rootpart="$ROOT_DISK"p2 ;;
-	*)
+	*) # *Los NVME tienen un esquema de ordenación diferente
 		bootpart="$ROOT_DISK"1
 		rootpart="$ROOT_DISK"2 ;;
 	esac
-	# Ajustamos / en función del tipo de disco
+	# Definimos el nombre de las particiones
+	# de nuestro disco /home (Si lo hay)
 	if [ "$home_partition" == "true" ]; then
 		local homepart
 		case "$HOME_DISK" in
 		*"nvme"*)
-			homepart="$HOME_DISK"p2 ;;
+			homepart="$HOME_DISK"p1 ;;
 		*)
-			homepart="$HOME_DISK"2 ;;
+			homepart="$HOME_DISK"1 ;;
 		esac
 	fi
-	# Vemos si / estará encriptado o no
+	# Mostraremos si el disco duro esta encriptado o no
 	if [ "$crypt_root" == "true" ]; then
-		roottype=LUKS
+		roottype="LUKS"
 	else
-		roottype=/
+		roottype="/"
+	fi
+	# Creamos el esquema que whiptail nos mostrará
+	scheme="/dev/$ROOT_DISK $(lsblk -dn -o size /dev/"$ROOT_DISK")
+	$bootmount  /dev/$bootpart
+	$roottype  /dev/$rootpart
+	"
+	if [ "$crypt_root" == "true" ]; then
+	scheme+="   /  /dev/mapper/root"
 	fi
 
 	if [ "$home_partition" == "true" ]; then
-	scheme="/dev/$ROOT_DISK
-	$bootmount /dev/$bootpart
-/dev/$HOME_DISK
-	/home /dev/$homepart
-$roottype /dev/$rootpart"
-	else
-	scheme="/dev/$ROOT_DISK
-	$bootmount /dev/$bootpart
-$roottype /dev/$rootpart"
-	fi
-
-	if [ "$crypt_root" == "true" ]; then
 	scheme+="
-	/ /dev/mapper/root"
+/dev/$HOME_DISK $(lsblk -dn -o size /dev/"$HOME_DISK")
+	/home  /dev/$homepart"
 	fi
-
-	whiptail --title "Confirmar particionado" --yesno "$scheme" 12 40
+	scheme+="
+Aceptar los cambios borrará el contenido de todos los discos mostrados"
+	# Mostramos el esquema para confirmar los cambios
+	whiptail --title "Confirmar particionado" --yesno "$scheme" 14 60 ||
+	script_exit
 }
 
+# Función para elegir como se formatearán nuestros discos
 scheme_setup(){
 local scheme_confirm="false"
 while [ "$scheme_confirm" == "false" ]; do
@@ -94,47 +106,84 @@ while [ "$scheme_confirm" == "false" ]; do
 	local root_selected="false"
 	while [ "$root_selected" == "false" ]; do
 	ROOT_DISK=$(whip_menu "Discos disponibles" "Selecciona un disco para la instalación:" \
-	"$(lsblk -d -o name,size,type | grep "disk" | awk '{print $1 " " $2}' | tr '\n' ' ')" ) && \
+	"$(lsblk -dn -o name,size | tr '\n' ' ')" ) && \
 	root_selected=true
 	done
-
 	# Preguntamos si queremos un disco dedicado para /home
 	if whip_yes "Partición /home" "¿Tiene un disco dedicado para su partición /home?"; then
 		home_partition=true
 	else
 		home_partition=false
 	fi
-
 	# Si queremos un disco para /home, elegimos cual
 	local home_selected="false"
 	[ "$home_partition" = true ] && \
 	while [ "$home_selected" == "false" ]; do
 	HOME_DISK=$(whip_menu "Discos disponibles" "Seleccione un disco para su partición /home:" \
-	"$(lsblk -d -o name,size,type | grep "disk" | awk '{print $1 " " $2}' | grep -v "$ROOT_DISK" | tr '\n' ' ')") && \
+	"$(lsblk -dn -o name,size | grep -v "$ROOT_DISK" | tr '\n' ' ')") && \
 	home_selected=true
 	done
-
 	# Elegimos si queremos encriptación en el disco /
 	if whip_yes "LUKS" "¿Desea usar encriptación para la partición /?"; then
 		crypt_root=true
 	else
 		crypt_root=false
 	fi
-
+	# Confirmamos los cambios
 	if scheme_show; then
 		scheme_confirm=true
 	else
-		whip_msg "ERROR" "Hubo un error al comprobar el esquema de particiones elegido, o el usuario cancelo la operación."
+		whip_msg "ERROR" "Hubo un error al comprobar el esquema de particiones elegido, o el usuario canceló la operación."
 	fi
 done
 }
 
-scheme_format(){
-	echo WIP
+root_encrypt(){
+while true; do
+	whip_msg "LUKS" "Se va a encriptar el disco /. A continuación se te pedirá la contraseña del disco"
+	cryptsetup luksFormat -q --verify-passphrase "/dev/$rootpart" && break
+	whip_msg "LUKS" "Hubo un error, deberá introducir la contraseña otra vez"
+done
 }
 
-scheme_format(){
-	echo WIP
+home_delete_confirm(){
+whiptail --title "$HOME_DISK" --yesno \
+"¿Desea borrar todos los datos de $HOME_DISK? Esto borrara toda la información que este contiene (Documentos, imágenes, videos, etc).\n
+En caso contrario se utilizará el disco duro tal cual esta ahora (Si esque ya se uso en otra instalación como /home)" 13 60
 }
 
+format_disks(){
+	# Borramos todas las firmas de nuestros discos
+	wipefs --all /dev/$ROOT_DISK
+	if [ "$home_partition" = "true" ] && home_delete_confirm; then
+		home_fresh="true"
+		wipefs --all /dev/$HOME_DISK
+	else
+		home_fresh="false"
+	fi
+	# Creamos nuestra tabla de particionado y partición de arranque
+	if [ "$PART_TYPE" == "msdos" ]; then # BIOS -> MBR
+		echo -e "label: dos\nstart=1MiB, size=512MiB, type=83\n" | sfdisk -f /dev/"$INSTALL_DISK" >/dev/null
+		mkfs.ext4 "/dev/$bootpart"
+	else # UEFI -> GPT
+		echo -e "label: gpt\nstart=1MiB, size=512MiB, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B\n" | \
+		sfdisk -f /dev/"$ROOT_DISK" >/dev/null; mkfs.fat -F32 "/dev/$bootpart"
+	fi
+	# Creamos la partición root
+	parted -s "/dev/$ROOT_DISK" mkpart primary 513MiB 100%
+	[ "$crypt_root" == "true" ] && root_encrypt
+}
+
+##########
+# SCRIPT #
+##########
+
+# Elegimos como se formatearán nuestros discos
 scheme_setup
+# Elegimos el formato que queremos para nuestros discos
+ROOT_FILESYSTEM=$(whip_menu "Sistema de archivos" "Selecciona el sistema de archivos para /:" \
+	"ext4" "Ext4" "btrfs" "Btrfs" "xfs" "XFS")
+[ "$home_partition" = true ] && HOME_FILESYSTEM=$(whip_menu "Sistema de archivos" "Selecciona el sistema de archivos para /home:" \
+	"ext4" "Ext4" "btrfs" "Btrfs" "xfs" "XFS")
+# Formateamos los discos
+format_disks
