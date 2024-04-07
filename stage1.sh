228c0dda5db06959cf3f9a5e67264a3ad502f600
aleister888 <pacoe1000@protonmail.com>
2024-04-07 12:21:56 +0200

    fix

--- stage1.sh
+++ stage1.sh
@@ -152,7 +152,12 @@ while true; do
 	cryptsetup luksFormat -q --verify-passphrase "/dev/$rootpart" && break
 	whip_msg "LUKS" "Hubo un error, deberá introducir la contraseña otra vez"
 done
+
+while true; do
+	whip_msg "LUKS" "Se te pedirá la contraseña para poder desencriptar el disco temporalmente y comenzar la instalación."
 	cryptsetup open "/dev/$rootpart" cryptroot
+	whip_msg "LUKS" "Hubo un error, deberá introducir la contraseña otra vez"
+done
 }
 
 home_delete_confirm(){
@@ -180,11 +185,10 @@ format_disks(){
 	# Creamos nuestra tabla de particionado y partición de arranque
 	if [ "$PART_TYPE" == "msdos" ]; then # BIOS -> MBR
 		echo -e "label: dos\nstart=1MiB, size=512MiB, type=83\n" | \
-		sfdisk -f "/dev/$ROOT_DISK"; mkfs.ext4 "/dev/$bootpart"
 	else # UEFI -> GPT
 		echo -e "label: gpt\nstart=1MiB, size=512MiB, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B\n" | \
-		sfdisk -f "/dev/$ROOT_DISK"; mkfs.fat -F32 "/dev/$bootpart"
 	fi
+	sfdisk -f "/dev/$ROOT_DISK"; mkfs.fat -F32 "/dev/$bootpart"
 
 	# Creamos la partición root
 	parted -s "/dev/$ROOT_DISK" mkpart primary 513MiB 100%
