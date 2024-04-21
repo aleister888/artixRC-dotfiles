45d6d844f4f1b80c6cefb882df39c0add7babc7f
aleister888 <pacoe1000@protonmail.com>
2024-04-21 16:20:46 +0200

    don't start picom on VMware

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -100,7 +100,7 @@ dbus-update-activation-environment --all
 pgrep polkit-gnome	|| /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 pgrep gnome-keyring	|| gnome-keyring-daemon -r -d &
 pgrep udiskie		|| udiskie -t -a &
-cat /sys/devices/virtual/dmi/id/product_name | grep Q35 || \
+cat /sys/devices/virtual/dmi/id/product_name | grep "Q35\|VMware" || \
 pgrep picom || picom -c -f --vsync --corner-radius 12 --config=$HOME/.config/picom/picom.conf &
 pgrep dwmblocks		|| dwmblocks &
 [ ! -e /sys/class/power_supply/BAT0 ] && sh -c 'pgrep x0vncserver || x0vncserver -localhost -SecurityTypes none' &
