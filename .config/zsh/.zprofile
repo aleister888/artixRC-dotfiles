7b541f6d1ef7c1c36d351d0ca44b83cf8c5150df
aleister888 <pacoe1000@protonmail.com>
2024-05-13 18:52:13 +0200

    Usar .profile en KDE Wayland

---
+++ .config/zsh/.zprofile
@@ -0,0 +1,1 @@
+/home/aleister/.dotfiles/.profile
--- update.sh
+++ update.sh
@@ -16,7 +16,8 @@ find $HOME/ -type d -exec chmod 700 {} +
 [ ! -d "$HOME/.cache" ] && mkdir -p "$HOME/.cache"
 sh -c "cd $HOME/.dotfiles && stow --target="${HOME}/.local/bin/" bin/" >/dev/null
 sh -c "cd $HOME/.dotfiles && stow --target="${HOME}/.config/" .config/" >/dev/null
-ln -s $HOME/.dotfiles/.profile $HOME/ 2>/dev/null
+ln -s $HOME/.dotfiles/.profile $HOME/.profile 2>/dev/null
+ln -s $HOME/.dotfiles/.profile $HOME/.config/zsh/.zprofile 2>/dev/null
 # Borrar enlaces rotos
 find "$HOME/.local/bin" -type l ! -exec test -e {} \; -delete
 find "$HOME/.config"    -type l ! -exec test -e {} \; -delete
