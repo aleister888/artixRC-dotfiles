ee1e6fe397508ce6a559d2fe11d0077d65a0c2f4
aleister888 <pacoe1000@protonmail.com>
2024-05-20 16:21:53 +0200

    Cambiar etiquetas de los espacios

--- dwm/1080.h
+++ dwm/1080.h
@@ -1,2 +1,2 @@
-static const char *fonts[] = { "Symbols Nerd Font:pixelsize=26","Iosevka Nerd Font:bold:pixelsize=24" };
-static const unsigned int gappx = 12;
+static const char *fonts[] = { "Symbols Nerd Font:pixelsize=24","Iosevka Nerd Font:bold:pixelsize=24" };
+static const unsigned int gappx = 16;
