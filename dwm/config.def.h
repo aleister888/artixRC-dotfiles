cfb301d2b4e757a663afb433442b25fb79cce3e1
aleister888 <pacoe1000@protonmail.com>
2024-10-28 13:02:58 +0100

    Cambiar icono de la etiqueta de KeepassXC

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -87,7 +87,7 @@ typedef struct {
 
 // Nombre de los espacios cuando estan vacios y cuando tienen ventanas. Layout por defecto
 static const char *tags[]	= { "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c"};
-static const char *alttags[]	= { "", "", "󰈹", "", "󰙯", "", "󰋅", "", "󰊗", "A", "B", "C"};
+static const char *alttags[]	= { "", "", "󰈹", "", "󰙯", "", "󰋅", "", "󰊗", "A", "B", "C"};
 static const int taglayouts[]	= {   2,   0,   0,   0,   2,   0,   0,   0,   0,   0,   0,   0};
 
 // Reglas pre-establecidas para colocar las ventanas
