4105d7a40b13650e42805a585b395479b5401ee8
aleister888 <pacoe1000@protonmail.com>
2024-11-05 17:45:09 +0100

    Arkenfox -> Mantener cookies

--- assets/configs/user-overrides.js
+++ assets/configs/user-overrides.js
@@ -2,7 +2,7 @@
 user_pref("identity.fxaccounts.enabled", false);
 user_pref("identity.fxaccounts.toolbar.enabled", false);
 
-// Permitir las extensions instaladas por el script
+// Permitir las extensiones instaladas por el script
 user_pref("extensions.autoDisableScopes", 0);
 
 // Desactivar pocket
@@ -20,17 +20,38 @@ user_pref("network.http.referer.XOriginPolicy", 0);
 // Mostrar siempre los marcadores
 user_pref("browser.toolbars.bookmarks.visibility", "always");
 
-// Barra de tareas del sistema
+// Usar la barra de ventana del sistema
 user_pref("browser.tabs.inTitlebar", 0);
+
+// Mantener sesiones iniciadas
+user_pref("browser.sessionstore.privacy_level", 0);
 
 // Mantener cookies hasta que estas expiren o se borren manualmente
 user_pref("network.cookie.lifetimePolicy", 0);
 user_pref("privacy.clearOnShutdown.cookies", false);
 
+// Borrar el resto de datos automáticamente
+user_pref("privacy.sanitize.sanitizeOnShutdown", true);
+user_pref("privacy.sanitize.clearOnShutdown.hasMigratedToNewPrefs2", false);
+user_pref("privacy.sanitize.cpd.hasMigratedToNewPrefs2", false);
+user_pref("privacy.clearOnShutdown.cache", true);
+user_pref("privacy.clearOnShutdown.siteSettings", true);
+user_pref("privacy.clearOnShutdown.history", true);
+user_pref("privacy.clearOnShutdown.formdata", true);
+user_pref("privacy.clearOnShutdown.downloads", true);
+user_pref("privacy.clearOnShutdown.sessions", true);
+user_pref("privacy.clearOnShutdown.offlineApps", true);
+
+// Pantalla de inicio
+user_pref("browser.startup.homepage", "about:home");
+user_pref("browser.startup.page", 1);
+user_pref("browser.newtabpage.enabled", true);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+
 // Desactivar notificaciones
 user_pref("dom.push.enabled", false);
 
-// Desactivar funciones expermientales
+// Desactivar funciones experimentales
 user_pref("browser.preferences.experimental", false);
 user_pref("browser.preferences.experimental.hidden", true);
 
@@ -39,3 +60,6 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
 // Desactivar el baúl de contraseñas
 user_pref("signon.rememberSignons", false);
+
+// Usar el mismo buscador para las ventanas privadas
+user_pref("browser.search.separatePrivateDefault", false);
