2a29f8cd6cc821e526b7144dd6ba31e445606ca6
aleister888 <pacoe1000@protonmail.com>
2025-02-18 19:32:37 +0100

    Cambiar iconos de bufferline

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -11,7 +11,7 @@ return {
 				diagnostics_indicator = function(count, level, diagnostics_dict, context)
 					local s = ""
 					for e, n in pairs(diagnostics_dict) do
-						local sym = e == "error" and " " or (e == "warning" and " " or " ")
+						local sym = e == "error" and " " or (e == "warning" and " " or " ")
 						s = s .. sym
 					end
 					return s
